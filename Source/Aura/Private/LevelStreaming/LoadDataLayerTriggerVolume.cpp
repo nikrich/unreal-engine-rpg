2e4153c603ffbacb7e27ff0449550eae2811767a
Jannik Richter <jannik811@gmail.com>
2025-06-28 12:39:10 +0200

    Working level loading

---
+++ Source/Aura/Private/LevelStreaming/LoadDataLayerTriggerVolume.cpp
@@ -0,0 +1,45 @@
+// Copyright Hungry Ghost
+
+
+#include "LevelStreaming/LoadDataLayerTriggerVolume.h"
+#include "Components/BoxComponent.h"
+#include "WorldPartition/DataLayer/DataLayerSubsystem.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include <Character/AuraCharacter.h>
+
+ALoadDataLayerTriggerVolume::ALoadDataLayerTriggerVolume()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    TriggerVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerVolume"));
+    RootComponent = TriggerVolume;
+    TriggerVolume->SetCollisionProfileName(TEXT("Trigger"));
+    TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &ALoadDataLayerTriggerVolume::OnOverlapBegin);
+}
+
+void ALoadDataLayerTriggerVolume::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ALoadDataLayerTriggerVolume::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+    const FHitResult& SweepResult)
+{
+    // Only Allow the Main Character to Load Levels
+    if (OtherActor && OtherActor->IsA<AAuraCharacter>())
+    {
+        if (UDataLayerSubsystem* DataLayerSubsystem = GetWorld()->GetSubsystem<UDataLayerSubsystem>())
+        {
+            if (DataLayerAssetToActivate) {
+                DataLayerSubsystem->SetDataLayerInstanceRuntimeState(DataLayerAssetToActivate, EDataLayerRuntimeState::Activated);
+            }
+
+			if (DataLayerAssetToDeactivate) {
+				DataLayerSubsystem->SetDataLayerInstanceRuntimeState(DataLayerAssetToDeactivate, EDataLayerRuntimeState::Unloaded);
+			}
+        }
+    }
+}
+
---
+++ Source/Aura/Public/LevelStreaming/LoadDataLayerTriggerVolume.h
@@ -0,0 +1,37 @@
+// Copyright Hungry Ghost
+
+#pragma once
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LoadDataLayerTriggerVolume.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class AURA_API ALoadDataLayerTriggerVolume : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ALoadDataLayerTriggerVolume();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data Layer")
+    TObjectPtr<UDataLayerAsset> DataLayerAssetToActivate;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data Layer")
+    TObjectPtr<UDataLayerAsset> DataLayerAssetToDeactivate;
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    UBoxComponent* TriggerVolume;};
